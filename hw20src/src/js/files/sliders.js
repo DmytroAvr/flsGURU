4c089afba0ab67b68585bbbc88deff2407df1fc7
Dmytro <dmytro9avr@gmail.com>
2022-11-25 11:39:01 +0200

    add swiper for second

--- hw20src/src/js/files/sliders.js
+++ hw20src/src/js/files/sliders.js
@@ -27,9 +27,9 @@ import "../../scss/base/swiper.scss";
 function initSliders() {
 	// Перечень слайдеров
 	// Проверяем, есть ли слайдер на стронице
-	if (document.querySelector('.swiper')) { // Указываем скласс нужного слайдера
+	if (document.querySelector('.slider-team')) { // Указываем скласс нужного слайдера
 		// Создаем слайдер
-		new Swiper('.swiper', { // Указываем скласс нужного слайдера
+		new Swiper('.slider-team', { // Указываем скласс нужного слайдера
 			// Подключаем модули слайдера
 			// для конкретного случая
 			modules: [Navigation],
@@ -73,8 +73,8 @@ function initSliders() {
 
 			// Кнопки "влево/вправо"
 			navigation: {
-				prevEl: '.swiper-button-prev',
-				nextEl: '.swiper-button-next',
+				prevEl: '.slider-buttons-arrRound__prev',
+				nextEl: '.slider-buttons-arrRound__next',
 			},
 			
 			// Брейкпоинты
@@ -97,6 +97,83 @@ function initSliders() {
 					spaceBetween: 30,
 				},
 			},
+			
+			// События
+			on: {
+
+			}
+		});
+	}
+
+	if (document.querySelector('.main-review__slider')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.main-review__slider', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 1,
+			spaceBetween: 30,
+			autoHeight: true,
+			speed: 800,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+		loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+			/*
+			// Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+			// Пагинация
+	
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: '.main-review__prev',
+				nextEl: '.main-review__next',
+			},
+			
+			// Брейкпоинты
+			//breakpoints: {
+			//	640: {
+			//		slidesPerView: 1,
+			//		spaceBetween: 0,
+			//		autoHeight: true,
+			//	},
+			//	768: {
+			//		slidesPerView: 2,
+			//		spaceBetween: 20,
+			//	},
+			//	992: {
+			//		slidesPerView: 3,
+			//		spaceBetween: 20,
+			//	},
+			//	1268: {
+			//		slidesPerView: 4,
+			//		spaceBetween: 30,
+			//	},
+			//},
 			
 			// События
 			on: {
