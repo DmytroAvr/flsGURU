8b8e887c0caff7338ce05875babbbd8cbd49842c
Dmytro <dmytro9avr@gmail.com>
2022-11-27 20:16:31 +0200

    454

---
+++ hw22/js/script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let hwPart1 = 'Я вивчаю JS';
+console.log(hwPart1);
+
+//hw 3
+//hw 3
+//1
+const userEyesColor = 'grey'; 
+//2
+let user, userName;
+userName = "Вася";
+user = userName;
+console.log(user);
