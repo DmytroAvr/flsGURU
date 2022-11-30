19ea7a14684f5a2371df92cf934984623d4ea327
Dmytro <dmytro9avr@gmail.com>
2022-11-30 07:59:16 +0200

    erre

--- hw22/js/script.js
+++ hw22/js/script.js
@@ -8,7 +8,34 @@ console.log(hwPart1);
 //1
 const userEyesColor = 'grey'; 
 //2
+
+/*
 let user, userName;
 userName = "Вася";
 user = userName;
 console.log(user);
+
+console.log('tests');
+console.log('----------------------');
+
+let someString = "DFGFK23 23423J4L23KJJG kjekrjkrej34 3lkjLKJOIH";
+console.log(someString);
+let stringSearch = someString.toLowerCase();
+console.log(stringSearch.slice(0, 10));
+console.log(stringSearch.replace("d", 0).includes('0'));
+console.log(stringSearch.startsWith("d"));
+console.log(stringSearch.endsWith("h"));
+console.log(stringSearch);
+console.log('----------------------');
+console.log('----------------------');
+
+*/
+
+let testFore = 'how are you';
+for (let i = 0; i < testFore.length; i++) {
+	console.log(testFore[i]);
+};
+//for (let index = 0; index < testFore.length; index++) {
+//	console.log(testFore[index]);
+	
+//}
