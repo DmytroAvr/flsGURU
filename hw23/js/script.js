3d2a307f2558a71bb135d764ff7f3586112faa99
Dmytro <dmytro9avr@gmail.com>
2022-12-01 08:33:57 +0200

    add body for hw23

---
+++ hw23/js/script.js
@@ -0,0 +1,24 @@
+'use strict'
+/*
+let a = 0;
+ 
+console.log( 0 && 58 || 18 && !!a++);
+console.log(a);
+console.log(!!--a);
+console.log(a);
+*/
+
+
+//if (10 > 8) {
+//	console.log('1');
+//} else if (2 > 1) {
+//	console.log('2');
+//}
+
+//console.log(!!false && 11 || !!'1' && 18);
+
+//let name = 0;
+//console.log(name ?? "щось");
+
+//6 LES
+
