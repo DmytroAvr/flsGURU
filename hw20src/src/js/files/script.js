5c6b7e56ac4c8e09cad57b4388632f9c494b22d8
Di <dmytro9avr@gmail.com>
2022-12-03 23:27:57 +0200

    subscribe part

--- hw20src/src/js/files/script.js
+++ hw20src/src/js/files/script.js
@@ -2,3 +2,10 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+let blogText = document.querySelectorAll(".about-latest-post__text-about");
+
+for (let i = 0; i < blogText.length; i++) {
+	blogText[i].innerText = blogText[i].innerText.slice(0, 100) + '...';
+	console.log(blogText[i]);
+}
