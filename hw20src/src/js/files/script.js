1f49f48a836a77d929d668331cfb7ba3dbc37e93
Di <dmytro9avr@gmail.com>
2022-12-04 23:42:48 +0200

    add listener of screen size

--- hw20src/src/js/files/script.js
+++ hw20src/src/js/files/script.js
@@ -9,3 +9,33 @@ for (let i = 0; i < blogText.length; i++) {
 	blogText[i].innerText = blogText[i].innerText.slice(0, 100) + '...';
 	console.log(blogText[i]);
 }
+
+
+//work with spoilers 
+//HTML Button
+{/*<button id="jsDisableButton" type="button" style="margin: 12px;">
+  JS Toggle Disable
+</button>*/}
+
+//JS Click Event Listener
+//const jsDisableButton = document.getElementById("jsDisableButton");
+//if (jsDisableButton) {
+//  jsDisableButton.addEventListener("click", () =>
+//    handleJSBtnClick(jsDisableButton)
+//  );
+//}
+const disableButtonSiteMap = document.querySelector(".footer-site-map__title");
+const disableButtonCourses = document.querySelector(".footer-courses__title");
+if (window.innerWidth < 610) {
+	disableButtonSiteMap.classList.toggle('_spoller-active');
+	disableButtonSiteMap.disabled=false;
+	
+	disableButtonCourses.classList.toggle('_spoller-active');
+	disableButtonCourses.disabled=false;
+}
+
+//JS Click Handler
+//const handleJSBtnClick = (jsDisableButton) => {
+//  jsDisableButton.disabled = true;
+//  console.log("JS Disabled");
+//};
