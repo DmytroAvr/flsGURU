58afe9227c581efb52266cf7868fa18bba57aee7
Dmytro <dmytro9avr@gmail.com>
2022-12-05 19:08:14 +0200

    hw23

--- hw23/js/script.js
+++ hw23/js/script.js
@@ -22,3 +22,41 @@ console.log(a);
 
 //6 LES
 
+//Циклы FOR и WHILE в JavaScript. Директивы break и continue
+
+//1.1
+		//for (let i = 1; i < 5; i++) {
+for (let i = 4; i < 5; i++) {
+	let counter = i;
+	console.log(counter);
+}
+//1.2
+	//let printTo4 = 1;
+	//while (printTo4 < 5) {
+	//	console.log(printTo4);
+	//	printTo4++;
+	//}
+
+//2 
+		//let num = 8;
+let num = 1;
+while (num) {  //work if true 0-false
+	console.log('while: ' + num);
+	--num;
+}
+//3
+		//let numWhile = 0;
+let numWhile = 2;
+while (numWhile < 3) { 
+	console.log('numWhile: ' + numWhile);
+	numWhile++;
+}
+//4
+MarkFor: for (let num = 0; num < 2; num++) {
+	for (let size = 0; size < 3; size++) {
+		console.log(size);
+			if (size == 1) {
+				break MarkFor;
+			}
+	}
+}
