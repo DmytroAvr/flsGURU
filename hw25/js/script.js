d57eaf49371044e9e8f9ec0424cd802d1f6983d5
Di <dmytro9avr@gmail.com>
2022-12-08 22:01:36 +0200

    feat: start with hw 25

---
+++ hw25/js/script.js
@@ -0,0 +1,33 @@
+'use strict'
+
+/*
+const someElement = document.querySelector(".les24-1");
+console.log(someElement);
+
+someElement.style.cssText = `
+padding-top: 400px;
+padding-bottom: 300px;
+color: red;
+`;
+//someElement.style.cssText = `padding-top: 10px`  полностью перепишет
+//
+//
+someElement.style.paddingTop = "10px";
+*/
+/*
+const someElement = document.querySelector(".les24-1");
+
+const elemStyle = getComputedStyle(someElement);
+// отримаємо багато різних параметрів. Вкажи що саме щукаєшь
+const accurateParam = parseInt(elemStyle.fontSize);
+console.log(accurateParam);
+*/
+
+/* 
+const someElement = document.querySelector(".les24-1");
+someElement.hasAttribute('name');
+someElement.getAttribute('name');
+someElement.setAttribute('name', 'value');
+someElement.removeAttribute('name');
+*/
+
