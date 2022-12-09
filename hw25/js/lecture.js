79bde93f599cee9e7e065d4b75a0610c52b30b17
Dmytro <dmytro9avr@gmail.com>
2022-12-09 21:11:47 +0200

    feat: foreach

--- hw25/js/lecture.js
+++ hw25/js/lecture.js
@@ -1,18 +1,34 @@
  "use strict"
 // use event listener
 
-const someLink = document.querySelector(".some-link");
-// стрелочная функция (для чего-то короткого\слабенького)
-//someLink.addEventListener('click', (event) => {
-//	console.log(event);
-//	console.log('hello');
-//}); 
+//const someLink = document.querySelector(".some-link");
+	// стрелочная функция (для чего-то короткого\слабенького)
+	//someLink.addEventListener('click', (event) => {
+	//	console.log(event);
+	//	console.log('hello');
+	//}); 
 
 // вызов функции. сколь угодно сложная
-someLink.addEventListener('click', func); 
-function func(event) {
-	console.log(event);
-	someLink.removeEventListener('click', func); 
-	console.log('Слухач подій видалено');
+//someLink.addEventListener('click', func); 
+//function func(event) {
+//	console.log(event);
+//	someLink.removeEventListener('click', func); 
+//	console.log('Слухач подій видалено');
+//	event.preventDefault();
+//}
 
-}
+//create listener for many 
+
+const someLinks = document.querySelectorAll(".some-link");
+someLinks.forEach(link => {
+	link.addEventListener('click', func); 
+	//add function inside foreach
+	function func(event) {
+		console.log(event);
+		console.log('Слухач подій видалено');
+		event.preventDefault();
+		link.removeEventListener('click', func); 
+	}
+});
+
+
