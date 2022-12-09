7476ce111243d6f49da1afc3b67220bf68bb6c6f
Dmytro <dmytro9avr@gmail.com>
2022-12-09 20:48:49 +0200

    feat: removeEventListemer

--- hw25/js/lecture.js
+++ hw25/js/lecture.js
@@ -3,13 +3,16 @@
 
 const someLink = document.querySelector(".some-link");
 // стрелочная функция (для чего-то короткого\слабенького)
-someLink.addEventListener('click', (event) => {
-	console.log(event);
-	console.log('hello');
-}); 
+//someLink.addEventListener('click', (event) => {
+//	console.log(event);
+//	console.log('hello');
+//}); 
 
 // вызов функции. сколь угодно сложная
 someLink.addEventListener('click', func); 
 function func(event) {
-	console.log(event);	
+	console.log(event);
+	someLink.removeEventListener('click', func); 
+	console.log('Слухач подій видалено');
+
 }
