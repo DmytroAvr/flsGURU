2144088f6eea95e1b4ff7fdcab1e3b81b2dd5fcb
Dmytro <dmytro9avr@gmail.com>
2022-12-09 20:42:11 +0200

    feat: event function

---
+++ hw25/js/lecture.js
@@ -0,0 +1,15 @@
+ "use strict"
+// use event listener
+
+const someLink = document.querySelector(".some-link");
+// стрелочная функция (для чего-то короткого\слабенького)
+someLink.addEventListener('click', (event) => {
+	console.log(event);
+	console.log('hello');
+}); 
+
+// вызов функции. сколь угодно сложная
+someLink.addEventListener('click', func); 
+function func(event) {
+	console.log(event);	
+}
