6cbfc525fd842653da1895968f9360b8366e62c8
Di <dmytro9avr@gmail.com>
2022-12-11 21:58:14 +0200

    init: add base for hw25

--- hw25/js/script.js
+++ hw25/js/script.js
@@ -1,33 +1,10 @@
-'use strict'
+"use strict"
 
-/*
-const someElement = document.querySelector(".les24-1");
-console.log(someElement);
-
-someElement.style.cssText = `
-padding-top: 400px;
-padding-bottom: 300px;
-color: red;
-`;
-//someElement.style.cssText = `padding-top: 10px`  полностью перепишет
-//
-//
-someElement.style.paddingTop = "10px";
-*/
-/*
-const someElement = document.querySelector(".les24-1");
-
-const elemStyle = getComputedStyle(someElement);
-// отримаємо багато різних параметрів. Вкажи що саме щукаєшь
-const accurateParam = parseInt(elemStyle.fontSize);
-console.log(accurateParam);
-*/
-
-/* 
-const someElement = document.querySelector(".les24-1");
-someElement.hasAttribute('name');
-someElement.getAttribute('name');
-someElement.setAttribute('name', 'value');
-someElement.removeAttribute('name');
-*/
-
+function addLoadedClass() {
+	if (!document.documentElement.classList.contains("loading")) window.addEventListener("load", (function() {
+		setTimeout((function() {
+			document.documentElement.classList.add("loaded");
+		}), 0);
+	}));
+}
+addLoadedClass();
