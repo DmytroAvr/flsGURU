b527fa13fc6db0c5dc1ee8e09e7e0f3a1c023d92
Dmytro <dmytro9avr@gmail.com>
2023-01-02 17:24:22 +0200

    feat: header

--- hw26Matao/src/js/files/sliders.js
+++ hw26Matao/src/js/files/sliders.js
@@ -23,9 +23,94 @@ import "../../scss/base/swiper.scss";
 // Повний набір стилів з node_modules
 // import 'swiper/css';
 
+
 // Ініціалізація слайдерів
+
 function initSliders() {
 	// Список слайдерів
+	
+
+	// Ініціалізація слайдерів
+	// hero slider
+	// Перевіряємо, чи є слайдер на сторінці
+		if (document.querySelector('.hero__slider')) { // Вказуємо склас потрібного слайдера
+			// Створюємо слайдер
+			new Swiper('.hero__slider', { // Вказуємо склас потрібного слайдера
+				// Підключаємо модулі слайдера
+				// для конкретного випадку
+				modules: [Navigation],
+				observer: true,
+				observeParents: true,
+				slidesPerView: 1,
+				spaceBetween: 0,
+				autoHeight: false,
+				speed: 800,
+	
+				//touchRatio: 0,
+				//simulateTouch: false,
+				//loop: true,
+				//preloadImages: false,
+				//lazy: true,
+	
+				/*
+				// Ефекти
+				effect: 'fade',
+				autoplay: {
+					delay: 3000,
+					disableOnInteraction: false,
+				},
+				*/
+	
+				// Пагінація
+				/*
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true,
+				},
+				*/
+	
+				// Скроллбар
+				/*
+				scrollbar: {
+					el: '.swiper-scrollbar',
+					draggable: true,
+				},
+				*/
+	
+				// Кнопки "вліво/вправо"
+				navigation: {
+					prevEl: 'hero__slider-arrow.swiper-button-prev',
+					nextEl: 'hero__slider-arrow.swiper-button-next',
+				},
+				/*
+				// Брейкпоінти
+				breakpoints: {
+					640: {
+						slidesPerView: 1,
+						spaceBetween: 0,
+						autoHeight: true,
+					},
+					768: {
+						slidesPerView: 2,
+						spaceBetween: 20,
+					},
+					992: {
+						slidesPerView: 3,
+						spaceBetween: 20,
+					},
+					1268: {
+						slidesPerView: 4,
+						spaceBetween: 30,
+					},
+				},
+				*/
+				// Події
+				on: {
+	
+				}
+			});
+		}
+
 	// Перевіряємо, чи є слайдер на сторінці
 	if (document.querySelector('.swiper')) { // Вказуємо склас потрібного слайдера
 		// Створюємо слайдер
